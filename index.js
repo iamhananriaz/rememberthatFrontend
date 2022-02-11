2520da81e1644bbe98088c9462b1391fcce425a6
Hanan Riaz <iamhanariaz@gmail.com>
2022-02-11 23:58:58 +0500

    front end

---
+++ index.js
@@ -0,0 +1,121 @@
+//  console.log("Hello");
+// var n1=1;
+// var n2=2;
+// console.log(n1+n2);
+
+// var str1= "hanan";
+// var a= true;
+// var b= false;
+// var arr1 = [1,2,3,'hmm',4]
+// console.log(str1) ;
+// console.log(a) ;
+// console.log(b) ;
+// console.log(arr1[2,3]) ;
+
+ 
+// function avg(a,b) {
+//     return (a+b)/2;
+// }
+// c1=avg(2,4);
+// c=avg(11,24);
+// console.log(c1,c);
+
+// var age=23;
+// if(age>44) {
+//     console.log('KId')
+// } 
+// else{
+//     console.log('no kid')
+// }
+ 
+ 
+ let arr= [1,2,3,4,5,6,7]
+//  let j=0;
+//  while (j<arr.length){
+//      console.log(arr[j])
+//      j++
+//  }
+
+// arr.forach(function(elements){
+//     console.log(elements);
+// })
+//  console.log(arr)
+//  for(var i=0;i<jjarr.length;i++){
+//      console.log(arr[i])
+//  }
+ 
+ 
+//  let arr= ["hanan", "fan" , 34 , null, true]
+// let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.getTime())
+// console.log(myDate.getDay())
+//  let pb= document.getElementById('pb');
+//  console.log(pb);
+
+//  let clss = document.getElementsByClassName('container');
+// //  console.log (clss);
+//  clss[0].classList.add('.bg-primary')
+ 
+
+// function clicked() {
+//     console.log("click")
+// }
+
+// window.onload= function() {
+//     console.log("loaded")
+// }
+
+// let prevHTML= document.querySelector('.chek').innerHTML;
+// firstcontainer.addEventListener('mouseup', function(){
+//     document.querySelector('.chek').innerHTML = prevHTML;
+//     console.log("mOUSE up")
+// })
+// firstcontainer.addEventListener('mousedown', function(){
+//     document.querySelector('.chek').innerHTML = " <b> THIS IS THE BOLD SCRIPT";
+//     console.log("mOUSE DOWN")
+//    })
+
+ //MOBILE MEDU JS   
+            const mobileBtn = document.getElementById('mobile-cta')
+            nav = document.querySelector('nav')
+            mobileBtnExit = document.getElementById('mobile-exit');
+
+            mobileBtn.addEventListener('click' , () => {
+                nav.classList.add('menu-btn');
+            })
+
+            mobileBtnExit.addEventListener('click' , () => {
+                nav.classList.remove('menu-btn');
+            })
+
+
+
+// clickfun = ()=> {
+//     document.getElementById('btt').innerHTML= "Black button"
+//     console.log("checking")
+// }
+
+//  setTimeout(clickfun , 2000);
+// clr = setInterval(clickfun , 2000)
+
+// localStorage.setItem("hanan",  "nan" ,  a : {this: "that"});
+obj= {hanan: 'nan', length: 1};
+jsn= JSON.stringify(obj);
+console.log(jso);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
